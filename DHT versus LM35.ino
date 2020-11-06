8b133507124f7a98de7cebbb60419fe59ab25b6d
FelipeSXavier <50845892+FelipeSXavier@users.noreply.github.com>
2020-11-06 17:27:54 -0300

    Update DHT versus LM35.ino

--- DHT versus LM35.ino
+++ DHT versus LM35.ino
@@ -1,8 +1,9 @@
 /*
+MEKANUS ROBÓTICA EDUCACIONAL
+TESTE COM SENSORES DE TEMPERATURA E HUMIDADE DHT11 E DHT22 EM COMPARAÇÃO COM O LM35
 Capta a temperatura dos sensores DHT11, DHT22 e LM35 e exibe o resultado no monitor serial
-
-
 */
+
 #include "DHT.h"
 #define DHT22_PIN 2
 #define DHT11_PIN A0
